a228782e79c24fd16a8f3447cecdeb234445f0d0
Barath P <p.barath1374@gmail.com>
2026-01-18 18:49:44 +0530

    staged for production

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import Sender from "./pages/Sender.jsx";
+import {Toaster} from "react-hot-toast";
+import DisplayText from "./components/receiver-components/DisplayText.jsx";
+
+function App() {
+    return (
+        <>
+            <Toaster
+                position="bottom-right"
+                toastOptions={{
+                    duration: 2000,
+                    style: {
+                        background:
+                            "linear-gradient(to top left, rgba(82,82,82,.6), rgba(113,113,113,.6), rgba(161,161,161,.6))",
+                        color: "#fff",
+                        backdropFilter: "blur(12px)",
+                    },
+                }}
+            />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/send" element={<Sender />} />
+                <Route path="/:code" element={<DisplayText />} />
+            </Routes>
+        </>
+    )
+}
+
+export default App
